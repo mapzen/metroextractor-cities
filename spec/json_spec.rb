73f69b0fdd45c32a0e8f3ce53c5452451882c6cf
Grant Heffernan <heffergm@gmail.com>
2014-09-16 07:40:56 -0400

    rearrange tests

--- spec/json_spec.rb
+++ spec/json_spec.rb
@@ -4,19 +4,6 @@ require 'json'
 require 'rainbow/ext/string'
 
 namespace :test do
-  desc 'Check for whitespace'
-  task :whitespace do
-    puts 'Checking cities.json for invalid whitespace'.color(:blue)
-    json_file = File.read('cities.json')
-    json_file.each_line do |line|
-      if line =~ /"\s+\d+|"\s+-|\d+\s+"/
-        puts 'Syntax Error!'.color(:red)
-        abort line.color(:yellow)
-      end
-    end
-    puts 'Whitespace OK'.color(:green)
-  end
-
   desc 'Validate JSON'
   task :json do
     json_file = File.read('cities.json')
