92d18f7cd2f5718011e9ccd790b2ff08ff3c58c4
Grant Heffernan <heffergm@gmail.com>
2015-01-25 18:14:18 -0500

    whitespace spec update: print linenum

--- spec/whitespace_spec.rb
+++ spec/whitespace_spec.rb
@@ -6,9 +6,12 @@ namespace :test do
   desc 'Check for whitespace'
   task :whitespace do
     puts 'Checking cities.json for invalid whitespace'.color(:blue)
+
+    linenum = 0
     CitiesJSON.each_line do |line|
+      linenum += 1
       if line =~ /"\s+\d+|"\s+-|\d+\s+"/
-        puts 'Syntax Error!'.color(:red)
+        puts "Syntax error on line number #{linenum}!".color(:red)
         abort line.color(:yellow)
       end
     end
