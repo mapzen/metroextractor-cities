7b65e8d4e5902d4915b1ed53ae4ba57ece644c71
Grant Heffernan <heffergm@gmail.com>
2015-02-12 10:10:13 -0500

    add bbox precision test clean up cities.json for new test

---
+++ spec/bbox_precision_spec.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env rake
+
+require 'json'
+require 'rainbow/ext/string'
+
+namespace :test do
+  desc 'Validate bounding box input is carried to 3 decimal place precision'
+  task :bbox_precision do
+    puts 'Validating cities.json bbox input is numeric'.color(:blue)
+    json = JSON.parse(CitiesJSON)
+
+    def decimal_precision?(input, area, limit)
+      places = input.to_s.split('.')[1].size.to_i
+      fail if places != limit
+    rescue
+      abort "Failure! Bounding box value for area \"#{area}\" is not denoted with 3 decimal places of precision : \"#{input}\".".color(:red)
+    end
+
+    boxes = %w(top left bottom right)
+    boxes.each do |box|
+      json['regions'].each do |region, a|
+        decimal_precision?(a['bbox'][box], region, 3)
+
+        a['cities'].each do |city, b|
+          decimal_precision?(b['bbox'][box], city, 3)
+        end
+      end
+    end
+    puts 'OK'.color(:green)
+  end
+end
--- tasks/test.rb
+++ tasks/test.rb
@@ -5,6 +5,7 @@ task :test do
   Rake::Task['test:bbox'].invoke
   Rake::Task['test:bbox_size'].invoke
   Rake::Task['test:bbox_numeric'].invoke
+  Rake::Task['test:bbox_precision'].invoke
   Rake::Task['test:json'].invoke
   Rake::Task['test:whitespace'].invoke
   Rake::Task['test:geojson'].invoke
